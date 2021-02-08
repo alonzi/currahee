62204a1d7137afc1a6ba3ccf9add93feef53775a
pete <pete.alonzi@gmail.com>
2021-02-07 22:32:27 -0500

    Add files via upload

--- viz.py
+++ viz.py
@@ -10,11 +10,11 @@ import tkinter as tk
 
 def request(prompt,var2set):
     ''' give a prompt and a variable to set with answer '''
-    
+
     master = tk.Tk()
     msg = tk.Message(master, text = prompt)
     msg.grid(row=0,column=0,columnspan=2)
-    msg.config(bg='olive drab', font=('courier', 12, 'bold'))
+    msg.config(bg='olive drab', font=('courier', 14, 'bold'),bd=0,width=300)
     tk.Label(master,text=var2set).grid(row=1,column=0)
     e1 = tk.Entry(master)
     e1.grid(row=1,column=1)
@@ -22,8 +22,16 @@ def request(prompt,var2set):
             text='Enter', 
             command=master.quit).grid(row=2, 
                                         column=0,
-                                        columnspan=2, 
+                                        columnspan=1, 
                                         pady=4,
                                         padx=4)
-    tk.mainloop()
+    tk.Button(master, 
+            text='Close', 
+            command=master.destroy).grid(row=2, 
+                                         column=1,
+                                         columnspan=1, 
+                                         pady=4,
+                                         padx=4)
+
+    tk.mainloop() # halts the python progression
     return e1.get()
