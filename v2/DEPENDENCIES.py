b94cf1a7191bd03a929bed93a58e0d8a48c1bd7b
alonzi <pete.alonzi@gmail.com>
2021-03-18 17:29:54 -0400

    running example

---
+++ v2/DEPENDENCIES.py
@@ -0,0 +1,5 @@
+# list of dependencies for curahee v2
+
+# pygame - http://inventwithpython.com/pygame/chapter1.html
+## https://www.pygame.org/wiki/GettingStarted
+#-----# python3 -m pip install -U pygame --user
